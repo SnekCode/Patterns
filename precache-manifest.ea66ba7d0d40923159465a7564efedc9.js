ce5fa1a29d736383393ee91dda8660d4bebd008f
snekcode <code.snakes@gmail.com>
2020-02-16 20:24:26 -1000

    Updates

---
+++ precache-manifest.ea66ba7d0d40923159465a7564efedc9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e07a4505c46d930792121c9d311a0aa3",
+    "url": "/Patterns/index.html"
+  },
+  {
+    "revision": "f24f1d667a317226c645",
+    "url": "/Patterns/static/js/2.8754ab93.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Patterns/static/js/2.8754ab93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "085e94f3cb35cc74c248",
+    "url": "/Patterns/static/js/main.3f072038.chunk.js"
+  },
+  {
+    "revision": "b2edcd55dbe4425eb620",
+    "url": "/Patterns/static/js/runtime-main.e5520d8a.js"
+  }
+]);
